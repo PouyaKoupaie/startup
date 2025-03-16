bd05437915f95cea2d15794712323a268cf2203a
Pouya <titanpooyanote@gmail.com>
2025-03-17 01:55:57 +0330

    shadcn and url searchParam aded

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 function Layout({children}: Readonly<{children: React.ReactNode}>) {
 
     return (
-        <main className='font-work-sans'>
+        <main className='font-work-sans bg-amber-50 h-[100vh]'>
             <Navbar/>
             {children}
         </main>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import SearchForm from "@/app/components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const params = await searchParams;
+  const filters = params.filters || '';
+  console.log("query", filters);
   return (
     <>
-      <h1 className="heading">HOME</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your goal,
+          <br />
+          connect with,
+          <br />
+          entrepreneuse
+        </h1>
+        <p className="sub-heading !max-w-3xl">Submit ideas, vote on pitches</p>
+        <SearchForm query={filters} />
+      </section>
     </>
   );
 }
